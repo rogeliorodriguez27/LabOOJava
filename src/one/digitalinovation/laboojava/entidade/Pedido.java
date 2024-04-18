4682d77adbb647185b0a0a00ddd7affd7c2ebd43
rogeliorodriguez27 <rogelio23rodriguez@gmail.com>
2024-04-17 21:40:09 -0300

    TODO RESOLVIDO class Pedido

--- src/one/digitalinovation/laboojava/entidade/Pedido.java
+++ src/one/digitalinovation/laboojava/entidade/Pedido.java
@@ -1,6 +1,5 @@
 package one.digitalinovation.laboojava.entidade;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -11,8 +10,34 @@ public class Pedido {
 
     //TODO Preencher esta classe
 
-    //codigo
-    //cliente
-    //produtos
-    //total
+    private String codigo;
+    private Cliente cliente;
+    private List<Produto> produtos;
+    private double total;
+    public String getCodigo() {
+        return codigo;
+    }
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+    public Cliente getCliente() {
+        return cliente;
+    }
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+    public List<Produto> getProdutos() {
+        return produtos;
+    }
+    public void setProdutos(List<Produto> produtos) {
+        this.produtos = produtos;
+    }
+    public double getTotal() {
+        return total;
+    }
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    
 }
